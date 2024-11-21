9a1f42cf2088a616a09585beba3a008c53724290
anttiey <hanuuny@gmail.com>
2024-11-21 16:05:59 +0900

    :wrench: ESLint 및 Prettier 기본 설정
    Co-authored-by: Minji <chosim-dvlpr@users.noreply.github.com>

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,22 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import react from '@vitejs/plugin-react-swc';
+import { defineConfig } from 'vite';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      '@': '/src',
+      '@/pages': '/src/pages',
+      '@/components': '/src/components',
+      '@/apis': '/src/apis',
+      '@/hooks': '/src/hooks',
+      '@/queries': '/src/queries',
+      '@/utils': '/src/utils',
+      '@/mocks': '/src/mocks',
+      '@/constants': '/src/constants',
+      '@/assets': '/src/assets',
+      '@/styles': '/src/styles',
+    },
+  },
+});
