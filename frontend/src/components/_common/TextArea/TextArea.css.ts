465ed2a14763e27d3263523437793889885fd505
bmj0053 <bmj05378@gmail.com>
2024-11-24 00:03:39 +0900

    ✨ TextArea 공통 컴포넌트 구현

---
+++ frontend/src/components/_common/TextArea/TextArea.css.ts
@@ -0,0 +1,9 @@
+import { style } from '@vanilla-extract/css';
+
+export const Layout = style({
+  display: 'flex',
+  flexDirection: 'column',
+  gap: '8px',
+});
+
+export const Label = style({}); // 추후 스타일 코드 추가
