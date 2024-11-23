4f8fd6b831b24b3cf80d115c02354634efd9499e
bmj0053 <bmj05378@gmail.com>
2024-11-23 21:23:24 +0900

    💄 Reset css 적용 및 불필요한 파일 삭제
    App.css 및 index.css 삭제

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,27 +1,7 @@
-import { RouterProvider, createBrowserRouter } from 'react-router-dom';
-
-import TreeMap from '@/pages/TreeMap/TreeMap';
+import './styles/global.css';
 
 const App = () => {
-  const router = createBrowserRouter(
-    [
-      {
-        path: '/',
-        element: <TreeMap />,
-      },
-    ],
-    {
-      future: {
-        v7_relativeSplatPath: true,
-        v7_fetcherPersist: true,
-        v7_normalizeFormMethod: true,
-        v7_partialHydration: true,
-        v7_skipActionErrorRevalidation: true,
-      },
-    },
-  );
-
-  return <RouterProvider router={router} future={{ v7_startTransition: true }} />;
+  return <></>;
 };
 
 export default App;
