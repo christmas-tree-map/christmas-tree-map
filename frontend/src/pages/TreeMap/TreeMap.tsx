431f95765ff9a950bc2e6ceed315833b2995acd7
anttiey <hanuuny@gmail.com>
2024-11-24 15:52:00 +0900

    :sparkles: 라우터 기본 설정

---
+++ frontend/src/pages/TreeMap/TreeMap.tsx
@@ -0,0 +1,11 @@
+import * as S from './TreeMap.css';
+
+const TreeMap = () => {
+  return (
+    <div id="map" className={S.Layout}>
+      Hello World!
+    </div>
+  );
+};
+
+export default TreeMap;
