e3b92b8a80ce30085f855867b1a7734f7650dd0b
bmj0053 <bmj05378@gmail.com>
2024-11-24 14:55:08 +0900

    💄 제출 페이지 디자인

--- frontend/src/components/_common/TextArea/TextArea.css.ts
+++ frontend/src/components/_common/TextArea/TextArea.css.ts
@@ -1,9 +1,25 @@
 import { style } from '@vanilla-extract/css';
 
+import { vars } from '@/styles/theme.css';
+
 export const Layout = style({
   display: 'flex',
   flexDirection: 'column',
   gap: '8px',
 });
 
-export const Label = style({}); // 추후 스타일 코드 추가
+export const Label = style({
+  font: vars.fonts.label,
+});
+
+export const Textarea = style({
+  width: '100%',
+  minHeight: '100px',
+  padding: '11px 10px',
+  borderRadius: '10px',
+
+  backgroundColor: vars.colors.grey[50],
+  color: vars.colors.grey[900],
+  font: vars.fonts.body,
+  wordBreak: 'break-all',
+});
