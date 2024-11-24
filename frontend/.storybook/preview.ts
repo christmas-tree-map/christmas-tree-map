4675ca15e64ad2211c15674d181756fe18e9374b
bmj0053 <bmj05378@gmail.com>
2024-11-24 16:15:10 +0900

    ✅ FeedItem, FeedList storybook 추가 및 storybook msw 연결

--- frontend/.storybook/preview.ts
+++ frontend/.storybook/preview.ts
@@ -1,7 +1,15 @@
 import type { Preview } from '@storybook/react';
 
+import { worker } from '../src/mocks/browser';
 import '../src/styles/global.css';
 import '../src/styles/reset.css';
+
+// Start the MSW worker
+worker.start();
+
+export const parameters = {
+  actions: { argTypesRegex: '^on[A-Z].*' },
+};
 
 const preview: Preview = {
   parameters: {
