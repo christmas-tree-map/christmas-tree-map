ffa5bf0f9164376608f0f964e904ca5b72faf554
bmj0053 <bmj05378@gmail.com>
2024-11-24 12:19:46 +0900

    ✨ postFeed API 구현 및 MSW 핸들러 추가

--- frontend/src/mocks/handlers.ts
+++ frontend/src/mocks/handlers.ts
@@ -16,4 +16,19 @@ export const handlers = [
   http.get('example.com/feeds', () => {
     return HttpResponse.json(mockFeeds);
   }),
+
+  http.post('example.com/feed', async ({ request }) => {
+    const data = (await request.json()) as { imageUrl: string; content: string };
+    const newData = {
+      id: mockFeeds.length + 1,
+      name: '토끼',
+      imageUrl: data.imageUrl,
+      content: data.content,
+      createdAt: new Date().toISOString(),
+      likeCount: 0,
+    };
+
+    mockFeeds.push(newData);
+    return HttpResponse.json({ status: 200 });
+  }),
 ];
