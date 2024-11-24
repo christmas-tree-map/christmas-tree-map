5bc7065c8cfb51aabcb6c79c020f181eb162a633
lemone <lejinhy@gmail.com>
2024-11-24 18:46:22 +0900

    feat: 트리 생성 api 구현 및 테스트 완료

---
+++ backend/src/main/java/com/christmas/tree/repository/TreeRepository.java
@@ -0,0 +1,6 @@
+package com.christmas.tree.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TreeRepository extends JpaRepository<TreeEntity, Long> {
+}
