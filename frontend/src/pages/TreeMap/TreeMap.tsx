f68184d160443c786f3d28f4072bb9fd66f358b4
anttiey <hanuuny@gmail.com>
2024-11-26 16:37:18 +0900

    :sparkles: 현재 위치에 따른 커스텀 마커 추가

--- frontend/src/pages/TreeMap/TreeMap.tsx
+++ frontend/src/pages/TreeMap/TreeMap.tsx
@@ -1,42 +1,50 @@
-import { useEffect } from 'react';
+import { useEffect, useRef } from 'react';
+
+import treeImage from '@/assets/tree.png';
 
 import * as S from './TreeMap.css';
 
-declare global {
-  interface Window {
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    kakao: any;
-  }
-}
+const { kakao } = window;
+
+const DEFAULT_LATITUDE = 37.5503;
+const DEFAULT_LONGITUDE = 126.9971;
+const DEFAULT_ZOOM_LEVEL = 3;
+
+const IMAGE_SIZE = new kakao.maps.Size(50, 55);
+const IMAGE_OPTIONS = { offset: new kakao.maps.Point(25, 55) };
 
 const TreeMap = () => {
+  const mapRef = useRef<HTMLDivElement | null>(null);
+
+  const treeImageMarker = new kakao.maps.MarkerImage(treeImage, IMAGE_SIZE, IMAGE_OPTIONS);
+
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const addMarker = (map: any, latitude: number, longitude: number) => {
+    const markerPosition = new kakao.maps.LatLng(latitude, longitude);
+    const marker = new kakao.maps.Marker({ position: markerPosition, image: treeImageMarker });
+
+    marker.setMap(map);
+  };
+
+  const initializeMap = (latitude: number, longitude: number) => {
+    const options = { center: new kakao.maps.LatLng(latitude, longitude), level: DEFAULT_ZOOM_LEVEL };
+    const map = new kakao.maps.Map(mapRef.current, options);
+
+    addMarker(map, latitude, longitude);
+  };
+
   useEffect(() => {
-    const container = document.getElementById('map');
-
     if (!navigator.geolocation) {
-      const options = {
-        center: new window.kakao.maps.LatLng(37.5503, 126.9971),
-        level: 3,
-      };
-
-      new window.kakao.maps.Map(container, options);
+      initializeMap(DEFAULT_LATITUDE, DEFAULT_LONGITUDE);
       return;
     }
 
-    navigator.geolocation.getCurrentPosition((position) => {
-      const latitude = position.coords.latitude;
-      const longitude = position.coords.longitude;
-
-      const options = {
-        center: new window.kakao.maps.LatLng(latitude, longitude),
-        level: 3,
-      };
-
-      new window.kakao.maps.Map(container, options);
-    });
+    navigator.geolocation.getCurrentPosition((position) =>
+      initializeMap(position.coords.latitude, position.coords.longitude),
+    );
   }, []);
 
-  return <div id="map" className={S.Layout} />;
+  return <div ref={mapRef} className={S.Layout} />;
 };
 
 export default TreeMap;
