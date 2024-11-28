79caf48f931ca0f828f13b47de02387c0e1b480b
lemone <lejinhy@gmail.com>
2024-11-28 19:12:41 +0900

    refactor: 예외 응답 클래스명 변경

--- backend/src/main/java/com/christmas/common/exception/CommonExceptionHandler.java
+++ backend/src/main/java/com/christmas/common/exception/CommonExceptionHandler.java
@@ -14,25 +14,25 @@ import lombok.extern.slf4j.Slf4j;
 public class CommonExceptionHandler {
 
     @ExceptionHandler(CustomException.class)
-    public ResponseEntity<ErrorResponse> handleCustomException(final CustomException e) {
+    public ResponseEntity<ExceptionResponse> handleCustomException(final CustomException e) {
         log.warn("cause & message: {}, invalidData: {}", NestedExceptionUtils.getMostSpecificCause(e), e.getInvalidData());
 
         return ResponseEntity.status(e.getStatus())
-                .body(ErrorResponse.from(e));
+                .body(ExceptionResponse.from(e));
     }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<ErrorResponse> handleValidException(final MethodArgumentNotValidException e) {
+    public ResponseEntity<ExceptionResponse> handleValidException(final MethodArgumentNotValidException e) {
         log.warn("cause: {}, message: {}", NestedExceptionUtils.getMostSpecificCause(e), e.getMessage());
         return ResponseEntity.status(HttpStatus.BAD_REQUEST)
-                .body(ErrorResponse.from(e));
+                .body(ExceptionResponse.from(e));
     }
 
     @ExceptionHandler(Exception.class)
-    public ResponseEntity<ErrorResponse> handleException(final Exception e) {
+    public ResponseEntity<ExceptionResponse> handleException(final Exception e) {
         log.error("cause: {}, message: {}", NestedExceptionUtils.getMostSpecificCause(e), e.getMessage());
 
         return ResponseEntity.status(CommonErrorCode.INTERNAL_SERVER_ERROR.getStatus())
-                .body(ErrorResponse.of(CommonErrorCode.INTERNAL_SERVER_ERROR));
+                .body(ExceptionResponse.of(CommonErrorCode.INTERNAL_SERVER_ERROR));
     }
 }
