03b65530d675d406be04637095ab81035cb113d6
anttiey <hanuuny@gmail.com>
2024-12-04 18:26:38 +0900

    :recycle: FloatingButton props 타입 변경

--- frontend/src/components/_common/FloatingButton/FloatingButton.tsx
+++ frontend/src/components/_common/FloatingButton/FloatingButton.tsx
@@ -1,13 +1,9 @@
 import EditIcon from '@/assets/edit.svg';
 import * as S from './FloatingButton.css';
 
-interface FloatingButtonProps {
-  onClick: () => void;
-}
-
-const FloatingButton = ({ onClick }: FloatingButtonProps) => {
+const FloatingButton = ({ onClick }: React.ButtonHTMLAttributes<HTMLButtonElement>) => {
   return (
-    <button className={S.Layout} onClick={onClick}>
+    <button type="button" className={S.Layout} onClick={onClick}>
       <img src={EditIcon} className={S.EditIcon} />
     </button>
   );
