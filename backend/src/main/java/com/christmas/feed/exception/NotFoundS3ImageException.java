b3d2e0f3cdf95241daa0b6c919a4ef514aeb3361
lemone <lejinhy@gmail.com>
2024-12-05 23:13:44 +0900

    feat: 이미지 및 s3 관련 커스텀 예외 추가

---
+++ backend/src/main/java/com/christmas/feed/exception/NotFoundS3ImageException.java
@@ -0,0 +1,13 @@
+package com.christmas.feed.exception;
+
+import java.util.Map;
+
+import com.christmas.common.exception.CustomErrorCode;
+import com.christmas.common.exception.CustomException;
+
+public class NotFoundS3ImageException extends CustomException {
+
+    public NotFoundS3ImageException(final CustomErrorCode errorCode, final Map<String, String> invalidData) {
+        super(errorCode, invalidData);
+    }
+}
