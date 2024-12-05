e483a5fde24e7fa9681badb1b6a9fcf7a0816e36
lemone <lejinhy@gmail.com>
2024-12-05 23:11:54 +0900

    feat: 피드 엔티티, 레포지토리 구현

---
+++ backend/src/main/java/com/christmas/feed/repository/entity/FeedEntity.java
@@ -0,0 +1,43 @@
+package com.christmas.feed.repository.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+import com.christmas.common.application.TimestampEntity;
+import com.christmas.tree.repository.TreeEntity;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+@Table(name = "feed")
+public class FeedEntity extends TimestampEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "tree_id")
+    private TreeEntity treeEntity;
+
+    @Column(name = "nickname", unique = true, nullable = false)
+    private String nickname;
+
+    @Column(name = "password", nullable = false)
+    private String password;
+
+    @Column(name = "content", nullable = false)
+    private String content;
+
+    @Column(name = "like_count", nullable = false)
+    private Long likeCount;
+}
