6bf68afd1a047509071ae4c10b252863b8867ee9
lemone <lejinhy@gmail.com>
2024-12-07 15:53:57 +0900

    feat: 피드 엔티티 및 dto에 제약 조건 추가

--- backend/src/main/java/com/christmas/feed/dto/FeedCreateRequest.java
+++ backend/src/main/java/com/christmas/feed/dto/FeedCreateRequest.java
@@ -1,4 +1,14 @@
 package com.christmas.feed.dto;
 
-public record FeedCreateRequest(long treeId, String content, String password) {
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+public record FeedCreateRequest(
+        @NotNull(message = "트리 id가 비어있습니다.")
+        Long treeId,
+        @NotBlank(message = "피드 내용이 비어있습니다.")
+        String content,
+        @NotBlank(message = "비밀번호가 비어있습니다.")
+        String password
+) {
 }
