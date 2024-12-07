d40b3e81092c12e9df286003b7f3ce9f3ea8849a
lemone <lejinhy@gmail.com>
2024-12-07 15:53:57 +0900

    feat: 피드 엔티티 및 dto에 제약 조건 추가

--- backend/src/main/java/com/christmas/feed/dto/FeedUpdateRequest.java
+++ backend/src/main/java/com/christmas/feed/dto/FeedUpdateRequest.java
@@ -1,10 +1,13 @@
 package com.christmas.feed.dto;
 
+import jakarta.validation.constraints.NotBlank;
+
 import org.springframework.web.multipart.MultipartFile;
 
 public record FeedUpdateRequest(
         String content,
         MultipartFile image,
+        @NotBlank(message = "비밀번호가 비어있습니다.")
         String password
 ) {
 }
