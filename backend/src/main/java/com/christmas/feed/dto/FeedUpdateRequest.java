a6d43cf881ef166186989dd655972e449b7f8dd9
lemone <lejinhy@gmail.com>
2024-12-07 21:25:41 +0900

    docs: 피드 api 문서화 추가

--- backend/src/main/java/com/christmas/feed/dto/FeedUpdateRequest.java
+++ backend/src/main/java/com/christmas/feed/dto/FeedUpdateRequest.java
@@ -1,13 +1,24 @@
 package com.christmas.feed.dto;
 
+import jakarta.annotation.Nullable;
 import jakarta.validation.constraints.NotBlank;
 
 import org.springframework.web.multipart.MultipartFile;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
+@Schema(name = "피드 수정 요청 바디")
 public record FeedUpdateRequest(
-        String content,
-        MultipartFile image,
+        @Schema(name = "비밀번호")
         @NotBlank(message = "비밀번호가 비어있습니다.")
-        String password
+        String password,
+
+        @Nullable
+        @Schema(name = "수정한 피드 내용. 수정을 했을 때만 값을 요청한다.", nullable = true)
+        String content,
+
+        @Nullable
+        @Schema(name = "수정한 이미지. 수정을 했을 때만 값을 요청한다.", nullable = true)
+        MultipartFile image
 ) {
 }
