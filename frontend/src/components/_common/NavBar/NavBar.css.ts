24fd8159f4ab7e96a01fd24cc28ec5cbbbf8fd2a
anttiey <hanuuny@gmail.com>
2024-12-12 15:41:59 +0900

    :lipstick: NavBar 디자인 변경

--- frontend/src/components/_common/NavBar/NavBar.css.ts
+++ frontend/src/components/_common/NavBar/NavBar.css.ts
@@ -2,17 +2,13 @@ import { style } from '@vanilla-extract/css';
 import { vars } from '@/styles/theme.css';
 
 export const Layout = style({
-  display: 'flex',
-  flexDirection: 'column',
-  gap: '9px',
-
   position: 'fixed',
   bottom: 0,
   left: 0,
   zIndex: 100,
 
   width: '100%',
-  padding: '0 20px',
+  padding: '0 16px',
 });
 
 export const Bar = style({
@@ -21,11 +17,12 @@ export const Bar = style({
   justifyContent: 'center',
 
   width: '100%',
-  height: '49px',
-  borderRadius: '49px',
+  height: '58px',
+  borderRadius: '58px',
 
   backgroundColor: '#ffffff',
   color: vars.colors.primary[800],
+  filter: 'drop-shadow(0px 3px 3px #00000040)',
 });
 
 export const Link = style({
