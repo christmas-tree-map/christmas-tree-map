20fd0115a9fbd159839cfd149d3be2cd48676ab4
bmj0053 <bmj05378@gmail.com>
2024-12-14 19:49:36 +0900

    ✅ Textarea storybook 추가

--- frontend/src/components/_common/TextArea/TextArea.stories.tsx
+++ frontend/src/components/_common/TextArea/TextArea.stories.tsx
@@ -8,24 +8,41 @@ const meta = {
   parameters: {
     layout: 'centered',
   },
+  argTypes: {
+    status: {
+      control: {
+        type: 'select',
+      },
+      options: ['default', 'error'],
+    },
+    maxLength: {
+      control: {
+        type: 'number',
+      },
+    },
+  },
   tags: ['autodocs'],
 } satisfies Meta<typeof TextArea>;
 
 export default meta;
 type Story = StoryObj<typeof TextArea>;
 
-const TextAreaWithHooks = () => {
+const TextAreaWithHooks = ({ status, errorMessage }: { status: 'default' | 'error'; errorMessage?: string }) => {
   const [value, setValue] = useState('');
 
   return (
-    <TextArea onChange={(e) => setValue(e.target.value)} value={value}>
+    <TextArea onChange={(e) => setValue(e.target.value)} value={value} status={status} errorMessage={errorMessage}>
       <TextArea.Label label="label" />
     </TextArea>
   );
 };
 
 export const Default: Story = {
-  render: () => <TextAreaWithHooks />,
+  render: () => <TextAreaWithHooks status="default" />,
+};
+
+export const Error: Story = {
+  render: () => <TextAreaWithHooks status="error" errorMessage="내용을 입력해 주세요." />,
 };
 
 const WithPrefilledTextComponent = () => {
