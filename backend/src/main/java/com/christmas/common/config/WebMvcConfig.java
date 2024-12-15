dd7da8accf210dc1425bb558a0cbd381c40fd1ac
lemone <lejinhy@gmail.com>
2024-12-15 18:05:46 +0900

    feat: 모든 엔드포인트 앞에 /api prefix 추가

--- backend/src/main/java/com/christmas/common/config/WebMvcConfig.java
+++ backend/src/main/java/com/christmas/common/config/WebMvcConfig.java
@@ -1,11 +1,19 @@
 package com.christmas.common.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.method.HandlerTypePredicate;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
+
+    @Override
+    public void configurePathMatch(final PathMatchConfigurer configurer) {
+        configurer.addPathPrefix("/api", HandlerTypePredicate.forAnnotation(RestController.class));
+    }
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
