ecf4ccdbbacb33377ea1cfed1125f4859590cf98
bmj0053 <bmj05378@gmail.com>
2024-12-23 02:08:12 +0900

    🐛 postFeed 및 msw 반환타입 수정

--- frontend/src/mocks/handlers.ts
+++ frontend/src/mocks/handlers.ts
@@ -1,17 +1,16 @@
 import { HttpResponse, http } from 'msw';
 import { API_URL } from '@/apis/requestAPI';
 import mockFeeds from './feeds.json';
-import mockTrees from './trees.json';
 
 export const handlers = [
-  http.get(`${API_URL}/feeds`, () => {
-    return HttpResponse.json({ status: 200, data: mockFeeds });
+  http.get(`${API_URL}/feed`, () => {
+    return HttpResponse.json(mockFeeds);
   }),
 
   http.post(`${API_URL}/feed`, async ({ request }) => {
     try {
       const formData = await request.formData();
-      const imageFile = formData.get('image') as File; //
+      const imageFile = formData.get('image') as File;
       const requestData = formData.get('request');
 
       if (!imageFile || !requestData) {
@@ -31,10 +30,8 @@ export const handlers = [
         treeId,
         password,
       };
-
       mockFeeds.push(newFeed);
-
-      return HttpResponse.json({ message: '피드가 성공적으로 추가되었습니다.', data: newFeed }, { status: 200 });
+      return HttpResponse.json(newFeed.id);
     } catch (error) {
       console.error('MSW 핸들러 에러:', error);
 
@@ -43,18 +40,18 @@ export const handlers = [
   }),
 
   http.post(`${API_URL}/tree`, async () => {
-    return HttpResponse.json({ status: 200, data: 1 });
+    return HttpResponse.json(1);
   }),
 
-  http.get(`${API_URL}/tree/filter`, async ({ request }) => {
-    const url = new URL(request.url);
-    const latitude = url.searchParams.get('latitude');
-    const longitude = url.searchParams.get('longitude');
+  // http.get(`${API_URL}/tree/filter`, async ({ request }) => {
+  //   const url = new URL(request.url);
+  //   const latitude = url.searchParams.get('latitude');
+  //   const longitude = url.searchParams.get('longitude');
 
-    if (!latitude || !longitude) {
-      return HttpResponse.json({ message: '위도와 경도가 필요합니다.' }, { status: 400 });
-    }
+  //   if (!latitude || !longitude) {
+  //     return HttpResponse.json({ message: '위도와 경도가 필요합니다.' }, { status: 400 });
+  //   }
 
-    return HttpResponse.json({ status: 200, data: mockTrees });
-  }),
+  //   return HttpResponse.json(mockTrees);
+  // }),
 ];
