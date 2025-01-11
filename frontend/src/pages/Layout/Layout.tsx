2a63a83c2689062e4b0a9bf6d11c1f69c762ba54
bmj0053 <bmj05378@gmail.com>
2025-01-11 17:14:24 +0900

    ✨ Header title 추가

--- frontend/src/pages/Layout/Layout.tsx
+++ frontend/src/pages/Layout/Layout.tsx
@@ -2,10 +2,14 @@ import { Outlet } from 'react-router-dom';
 import Header from '@/components/_common/Header/Header';
 import NavBar from '@/components/_common/NavBar/NavBar';
 
-const Layout = () => {
+interface LayoutProps {
+  title?: string;
+}
+
+const Layout = ({ title = '' }: LayoutProps) => {
   return (
     <>
-      <Header />
+      <Header title={title} />
       <Outlet />
       <NavBar />
     </>
