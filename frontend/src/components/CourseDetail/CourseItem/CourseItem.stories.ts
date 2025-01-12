baafd7be81cd70e3d99c38d7e9dd3aa3759edd0d
anttiey <hanuuny@gmail.com>
2025-01-12 20:30:16 +0900

    :sparkles: CourseItem 스토리북 추가

---
+++ frontend/src/components/CourseDetail/CourseItem/CourseItem.stories.ts
@@ -0,0 +1,20 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import CourseItem from '@/components/CourseDetail/CourseItem/CourseItem';
+
+const meta = {
+  title: 'CourseDetail/CourseItem',
+  component: CourseItem,
+  tags: ['autodocs'],
+} satisfies Meta<typeof CourseItem>;
+
+export default meta;
+
+type Story = StoryObj<typeof CourseItem>;
+
+export const Default: Story = {
+  args: {
+    title: '가게 이름',
+    address: '가가시 나나구 다다동 라라',
+    phone: '010-1234-5678',
+  },
+};
