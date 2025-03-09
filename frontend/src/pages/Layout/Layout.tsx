257bd550262ccf858dde96b8008d2b056d37c942
bmj0053 <bmj05378@gmail.com>
2025-03-09 20:57:49 +0900

    ✨ GlobalErrorBoundary 설정

--- frontend/src/pages/Layout/Layout.tsx
+++ frontend/src/pages/Layout/Layout.tsx
@@ -1,6 +1,7 @@
 import { Outlet } from 'react-router-dom';
 import Header from '@/components/_common/Header/Header';
 import NavBar from '@/components/_common/NavBar/NavBar';
+import GlobalErrorBoundary from '../Error/ErrorBoundary/GlobalErrorBoundary';
 
 interface LayoutProps {
   title?: string;
@@ -10,11 +11,11 @@ interface LayoutProps {
 
 const Layout = ({ title = '', backgroundColor = 'transparent', isSticky = false }: LayoutProps) => {
   return (
-    <>
+    <GlobalErrorBoundary>
       <Header title={title} backgroundColor={backgroundColor} />
       <Outlet />
       <NavBar isSticky={isSticky} />
-    </>
+    </GlobalErrorBoundary>
   );
 };
 
