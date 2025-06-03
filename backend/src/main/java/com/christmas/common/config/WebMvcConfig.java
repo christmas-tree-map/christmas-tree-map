beec30f5370b1e85aae7da49c9e07a238356dde4
lemone <lejinhy@gmail.com>
2025-06-03 20:52:39 +0900

    chore: cors 전부 허용 확인

--- backend/src/main/java/com/christmas/common/config/WebMvcConfig.java
+++ backend/src/main/java/com/christmas/common/config/WebMvcConfig.java
@@ -18,7 +18,8 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("https://merrytreesmas.site", "https://fe-dev--merrytreesmas.netlify.app", "http://localhost:5173")
+//                .allowedOrigins("https://merrytreesmas.site", "https://fe-dev--merrytreesmas.netlify.app", "http://localhost:5173")
+                .allowedOrigins("*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH", "HEAD")
                 .allowedHeaders("*")
                 .allowCredentials(true);
