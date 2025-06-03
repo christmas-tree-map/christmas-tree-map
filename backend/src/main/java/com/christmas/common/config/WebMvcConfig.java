af075b7789c219a664a109d52c6f38e29b24641c
lemone <lejinhy@gmail.com>
2025-06-03 22:40:41 +0900

    chore: cors 테스트 통과 후 되돌리기

--- backend/src/main/java/com/christmas/common/config/WebMvcConfig.java
+++ backend/src/main/java/com/christmas/common/config/WebMvcConfig.java
@@ -19,7 +19,6 @@ public class WebMvcConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("https://merrytreesmas.site", "https://fe-dev--merrytreesmas.netlify.app", "http://localhost:5173")
-//                .allowedOrigins("*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH", "HEAD")
                 .allowedHeaders("*")
                 .allowCredentials(true);
