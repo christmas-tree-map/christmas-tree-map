eb528355a639f27036e48d324d57e7daa10d8d6f
lemone <lejinhy@gmail.com>
2025-08-27 01:17:17 +0900

    feat: 시간 반환 시 한국 시간으로 변환

--- backend/src/main/java/com/christmas/common/application/TimestampEntity.java
+++ backend/src/main/java/com/christmas/common/application/TimestampEntity.java
@@ -3,22 +3,32 @@ package com.christmas.common.application;
 import jakarta.persistence.Column;
 import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
-import java.time.OffsetDateTime;
-import lombok.Getter;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
-@Getter
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener.class)
 public class TimestampEntity {
 
     @CreatedDate
     @Column(name = "created_at", nullable = false)
-    private OffsetDateTime createdAt;
+    private LocalDateTime createdAt;
 
     @LastModifiedDate
     @Column(name = "updated_at", nullable = false)
-    private OffsetDateTime updatedAt;
+    private LocalDateTime updatedAt;
+
+    public ZonedDateTime getCreatedAtSeoul() {
+        return createdAt.atZone(ZoneId.of("UTC"))
+                .withZoneSameInstant(ZoneId.of("Asia/Seoul"));
+    }
+
+    public ZonedDateTime getUpdatedAtSeoul() {
+        return updatedAt.atZone(ZoneId.of("UTC"))
+                .withZoneSameInstant(ZoneId.of("Asia/Seoul"));
+    }
 }
