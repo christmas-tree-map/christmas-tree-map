23c7352eea91a6bc37a7c6b4392c7ed0fd7f3949
lemone <lejinhy@gmail.com>
2025-08-27 00:37:30 +0900

    feat: 타임스탬프 한국 시간대로 변경

--- backend/src/main/java/com/christmas/common/application/TimestampEntity.java
+++ backend/src/main/java/com/christmas/common/application/TimestampEntity.java
@@ -1,16 +1,13 @@
 package com.christmas.common.application;
 
-import java.time.LocalDateTime;
-
 import jakarta.persistence.Column;
 import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
-
+import java.time.OffsetDateTime;
+import lombok.Getter;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
-
-import lombok.Getter;
 
 @Getter
 @MappedSuperclass
@@ -19,9 +16,9 @@ public class TimestampEntity {
 
     @CreatedDate
     @Column(name = "created_at", nullable = false)
-    private LocalDateTime createdAt;
+    private OffsetDateTime createdAt;
 
     @LastModifiedDate
     @Column(name = "updated_at", nullable = false)
-    private LocalDateTime updatedAt;
+    private OffsetDateTime updatedAt;
 }
