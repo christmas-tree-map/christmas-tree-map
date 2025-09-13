bd23603b24c53a655d15a580da2cacc67d0edb41
bmj0053 <bmj05378@gmail.com>
2025-09-13 22:23:47 +0900

    feat: useTreesQuery에서 loading 상태 받을 수 있도록 추가

--- frontend/src/queries/Tree/useTreesQuery.ts
+++ frontend/src/queries/Tree/useTreesQuery.ts
@@ -3,12 +3,12 @@ import { getTrees } from '@/apis/tree';
 import { TREE_KEYS } from '@/queries/queryKeys';
 
 const useTreesQuery = ({ latitude, longitude }: { latitude: number; longitude: number }) => {
-  const { data, isSuccess } = useQuery({
+  const { data, isSuccess, isLoading } = useQuery({
     queryKey: [TREE_KEYS.TREES, latitude, longitude],
     queryFn: () => getTrees({ latitude, longitude }),
   });
 
-  return { trees: data ?? [], isSuccess };
+  return { trees: data ?? [], isSuccess, isLoading };
 };
 
 export default useTreesQuery;
